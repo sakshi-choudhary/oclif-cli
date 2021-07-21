025a1b302156198144e6a74544918baf860369e3
Sakshi <choudharysakshi023@gmail.com>
2021-07-21 12:51:28 +0530

    🐛 fix nachos cmd bug

--- src/commands/nachos.js
+++ src/commands/nachos.js
@@ -1,39 +1,47 @@
-const { Command } = require("@oclif/command");
+const { Command, flags } = require("@oclif/command");
 const { cli } = require("cli-ux");
+const inquirer = require("inquirer");
 
 class nachosCommand extends Command {
-  static args = [
-    {
-      name: "file", // name of arg to show in help and reference with args[name]
-      required: false, // make the arg required with `required: true`
-      description: "output file", // help description
-      hidden: false, // hide this arg from help
-      //parse: input, // instead of the user input, return a different value
-      default: "is good", // default value if no arg input
-      //options: ["a", "b"], // only allow input to be from a discrete set
-    },
-  ];
+  static flags = {
+    nachos: flags.string({ options: ["peri peri", "butter", "pickle"] }),
+  };
   async run() {
-    const { argv } = this.parse(nachosCommand);
-    const name = await cli.prompt("What nachos do you want?");
+    const { flags } = this.parse(nachosCommand);
+    let nachos = flags.nachos;
+    if (!nachos) {
+      let responses = await inquirer.prompt([
+        {
+          name: "nachos",
+          message: "select a nachos",
+          type: "list",
+          choices: [
+            { name: "peri peri" },
+            { name: "butter" },
+            { name: "pickle" },
+          ],
+        },
+      ]);
+      nachos = responses.nachos;
+    }
 
-    const nachos = [
+    const nachosData = [
       {
-        name: "abc1",
+        name: "peri peri",
         price: "20",
       },
       {
-        name: "abc2",
+        name: "butter",
         price: "25",
       },
       {
-        name: "abc3",
+        name: "pickle",
         price: "30",
       },
     ];
 
-    const price = nachos.find((p) => p.name === name).price;
-    this.log("Pay Rs.", `${price}`);
+    const price = nachosData.find((p) => p.name === nachos).price;
+    this.log(`Pay Rs. ${price}`);
   }
 }
 nachosCommand.description = "this is nachos command description";
